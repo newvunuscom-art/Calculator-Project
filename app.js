84182e7b630793e91c0033730e9302d95b506e7b
Moyou <newvunus1234.com@gmail.com>
2026-07-13 09:42:36 +0700

    add code javascript

---
+++ app.js
@@ -0,0 +1,26 @@
+var oparators = ["+", "-", "/", "*"];
+
+var box = null;
+var last_oparation_history = null;
+var oparator = null;
+var equal = null;
+var dot = null;
+
+var firstNum = true;
+
+var numbers = [];
+var oparator_value;
+var last_button;
+var calc_oparator;
+
+var total;
+
+var key_combination =[]
+var resultDisplayed = false;
+
+function button_number (button){
+    oparator = document.getElementsByClassName("oparator");
+    box = document.getElementById("box");
+    
+}
+
